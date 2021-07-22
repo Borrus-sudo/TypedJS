fcf2d9dcccec9dd4e5bf9606b57874dfbd105ab0
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-07-22 20:05:59 +0530

    chore: add new files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 export * from "./types/useInt.js";
+export * from "./types/useFloat.js";
+export * from "./types/useBoolean.js";
+export * from "./types/useString.js";
+export * from "./types/useObject.js";
+export * from "./types/useArray.js";
+export * from "./types/defineInterface.js ";
+export * from "./types/defineTypes.js";
