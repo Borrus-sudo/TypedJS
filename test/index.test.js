9dd5a4132ace1de8d6fcba0252b6c1e3373cc22d
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-07-22 18:08:17 +0530

    update test

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,11 @@
 import { useInt } from "../src/index.js";
 console.log(useInt);
 describe("My useInt function", () => {
-    it("has to have name set to Jinmay", () => {
+    it("should run correctly", () => {
         expect(useInt(10)).toBe(10);
     });
-    it("has to have surname set to Jhaveri", () => {
+    it("should run correctly", () => {
         expect(useInt(11)).toBe(11);
     });
+
 });
