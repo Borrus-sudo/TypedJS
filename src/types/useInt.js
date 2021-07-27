80b19e393fdd0f25bf6e5bac0490567ca5f5f4f5
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-07-27 18:11:10 +0530

    feat(useInt): add more support for reactivity

--- src/types/useInt.js
+++ src/types/useInt.js
@@ -1,29 +1,31 @@
 // import throwError from "./throwError";
 export function useInt(value, options = {}) {
-    if (!Number.isInteger(value))
+    if (!Number.isInteger(value) ||
+        (typeof value === "function" && !Number.isInteger(value()))
+    )
         throw new TypeError(
             `TypeError: Value of type non integer cannot be assigned to type integer`
         );
-    const dependencyTracker = new WeakMap();
+
+    const evalFunc = typeof value === "function" ? value : () => value;
     const payload = new Proxy(
         Object.defineProperties({}, {
-            _value: {
-                value,
+            value: {
+                get value() {
+                    return this._eval();
+                },
                 writable: true,
                 configurable: false,
                 enumerable: false,
             },
-            [Symbol.toPrimitive]: {
-                value: function() {
-                    return this._value;
-                },
-                configurable: true,
+            _eval: {
+                value: evalFunc,
                 writable: true,
-                enumerable: true,
+                configurable: false,
+                enumerable: false,
             },
         }), {
-            get(target, prop, receiver) {
-                dependencyTracker.set(target, receiver);
+            get(target, prop, _receiver) {
                 console.log({ prop });
                 console.log(typeof target[prop]);
                 if (typeof prop === "string")
@@ -31,14 +33,18 @@ export function useInt(value, options = {}) {
                         throw new Error("Access Error: Property not accessible");
                     }
                 const caller = target[prop];
-                if (typeof caller === "function") {
-                    return caller.bind(target);
-                }
-                return Reflect.get(...arguments);
+
+                return typeof caller === "function" ?
+                    caller.bind(target) :
+                    Reflect.get(...arguments);
             },
             set(_target, _prop, val) {
-                if (Number.isInteger(val)) {
-                    return Reflect.set(...arguments);
+                if (
+                    Number.isInteger(val) ||
+                    (typeof val === "function" && Number.isInteger(val()))
+                ) {
+                    const newVal = typeof val === "function" ? val : () => val;
+                    return Reflect.set(_target, "evalFunc", newVal);
                 } else {
                     throw new TypeError(
                         `TypeError: Value of type non integer cannot be assigned to type integer`
